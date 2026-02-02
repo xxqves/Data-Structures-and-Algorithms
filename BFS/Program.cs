a3fb19f49befac03bd1ce7b66ca46ff0a3ed54cf
Danil <Danil@DESKTOP-ETI2JS2>
2026-02-02 03:20:38 +0300

    add bfs

---
+++ BFS/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace BFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+            graph["Me"] = new List<string> { "Alice", "Bob", "Clar" };
+            graph["Bob"] = new List<string> { "Anuj", "Peggi" };
+            graph["Alice"] = new List<string> { "Peggi" };
+            graph["Clar"] = new List<string> { "Tom", "Jhonny" };
+            graph["Anuj"] = new List<string>();
+            graph["Peggi"] = new List<string>();
+            graph["Tom"] = new List<string>();
+            graph["Jhonny"] = new List<string>();
+
+            foreach (var node in graph)
+            {
+                Console.WriteLine($"{node.Key}: {string.Join(", ", node.Value)}");
+            }
+
+            Queue<string> searchQueue = new Queue<string>(graph["Me"]);
+
+            List<string> searched = new List<string>();
+
+            while (searchQueue.Count > 0)
+            {
+                string person = searchQueue.Dequeue();
+
+                if (!searched.Contains(person))
+                {
+                    Console.WriteLine($"Проверяем {person}...");
+
+                    if (PersonIsSeller(person))
+                    {
+                        Console.WriteLine($"Найден продавец манго: {person}");
+                        return;
+                    }
+                    else
+                    {
+                        foreach (var friend in graph[person])
+                        {
+                            searchQueue.Enqueue(friend);
+                        }
+
+                        searched.Add(person);
+                    }
+                }
+            }
+
+            Console.WriteLine("Продавец манго не найден!");
+        }
+
+        static bool PersonIsSeller(string name)
+        {
+            return name.EndsWith("m", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
--- CustomHashTable/Program.cs
+++ CustomHashTable/Program.cs
@@ -4,13 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var table = new HashTable<string, int>();
-            table.Add("hello", 1);
-            Console.WriteLine(table.TryGetValue("privet", out int _));
-
-            table["privet"] = 2;
-
-            Console.WriteLine(table.TryGetValue("privet", out int _)); 
+            
         }
     }
 }
